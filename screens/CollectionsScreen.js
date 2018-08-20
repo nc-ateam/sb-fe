f5dbe0046415af21a8d0225839819cd926ae6c71
rzhekova <rzhekova@gmail.com>
2018-08-20 10:45:57 +0100

    adds CollectionsScreen

---
+++ screens/CollectionsScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "@shoutem/ui";
+
+const CollectionsScreen = () => {
+  return (
+    <View>
+      <Text>Collections screen</Text>
+    </View>
+  );
+};
+
+export default CollectionsScreen;
