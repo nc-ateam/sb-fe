5f78f9d8465f07f60ad9587602d259889de1c6f4
rzhekova <rzhekova@gmail.com>
2018-08-20 11:04:15 +0100

    adds UserSettings screen

---
+++ screens/UserSettings.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View } from "@shoutem/ui";
+
+class UserSettings extends Component {
+  render() {
+    return (
+      <View>
+        <Text>User settings screen</Text>
+      </View>
+    );
+  }
+}
+
+export default UserSettings;
