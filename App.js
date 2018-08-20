77e98407415048efeb38f211bcbf7087a8e6fd3d
rzhekova <rzhekova@gmail.com>
2018-08-20 10:57:37 +0100

    creates MapScreen

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import { StyleSheet, Text, View } from "react-native";
 import LogInScreen from "./screens/LogInScreen";
 
 export default class App extends React.Component {
@@ -7,12 +6,3 @@ export default class App extends React.Component {
     return <LogInScreen />;
   }
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center"
-//   }
-// });
---
+++ screens/MapScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View } from "@shoutem/ui";
+
+class MapScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Map and achievements screen</Text>
+      </View>
+    );
+  }
+}
+
+export default MapScreen;
