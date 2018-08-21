73c618e3b6c9ad73a4708a19414910ebd1def240
John Harrington <32112213+jxh01753@users.noreply.github.com>
2018-08-21 12:48:11 +0100

    Implements a mock Api for getAllCountries

--- screens/UserSettingsScreen.js
+++ screens/UserSettingsScreen.js
@@ -1,16 +1,30 @@
-import React, { Component } from "react";
-import { View, NavigationBar, Icon, Title, Text, Button } from "@shoutem/ui";
-import { ScrollView } from "react-native";
+import React, { Component } from 'react';
+import { View, NavigationBar, Icon, Title, Text, Button } from '@shoutem/ui';
+import { ScrollView } from 'react-native';
+import CountryApi from '../api/mockCountryApi';
 
 class UserSettingsScreen extends Component {
+  state = {
+    data: {}
+  };
+
+  // This is an example of calling the mockApi, this will need to be changed to something like Axios or whatever we choose for the remote API.
+  componentDidMount = async () => {
+    let data = await CountryApi.getAllCountries();
+    this.setState({
+      data
+    });
+    console.log(this.state.data);
+  };
+
   render() {
     return (
       <View style={{ flex: 1 }}>
         <ScrollView
           contentContainerStyle={{
             flex: 1,
-            alignItems: "center",
-            justifyContent: "center"
+            alignItems: 'center',
+            justifyContent: 'center'
           }}
         >
           <View>
