5accc704dda5b890a383b548a8d9608f5c912764
rzhekova <rzhekova@gmail.com>
2018-08-21 17:34:53 +0100

    adds map to mapscreen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,32 +1,35 @@
 import React, { Component } from "react";
-import { View, NavigationBar, Icon, Title, Text } from "@shoutem/ui";
-import { ScrollView } from "react-native";
+import { View, NavigationBar, Icon, Title, Button } from "@shoutem/ui";
+import { Dimensions, StatusBar } from "react-native";
+import { MapView } from "expo";
 
+StatusBar.setBarStyle("light-content", true);
 class MapScreen extends Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <ScrollView
-          contentContainerStyle={{
-            flex: 1,
-            alignItems: "center",
-            justifyContent: "center"
+        <MapView
+          style={{ flex: 1, height: Dimensions.get("window").height }}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
           }}
-        >
-          <View>
-            <Text>Map and achievements go here</Text>
-          </View>
-        </ScrollView>
-
+        />
         {/* navigation bar should stay at the bottom otherwise {flex: 1} causes button to not work */}
         <NavigationBar
+          styleName="clear"
           leftComponent={
-            <Icon
-              onPress={() => this.props.navigation.openDrawer()}
-              name="sidebar"
-            />
+            <Button
+              onPress={
+                () => this.props.navigation.openDrawer() // styleName="clear"
+              }
+            >
+              <Icon style={{ color: "black" }} name="sidebar" />
+            </Button>
           }
-          centerComponent={<Title>Map</Title>}
+          centerComponent={<Title style={{ color: "black" }}>Map</Title>}
         />
       </View>
     );
