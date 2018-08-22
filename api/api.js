bdccf857ccc4ccb6295ab6063dcf8029697fe030
rzhekova <rzhekova@gmail.com>
2018-08-22 15:11:43 +0100

    fetches all countries

--- api/api.js
+++ api/api.js
@@ -7,8 +7,14 @@ export const fetchLandmarksByCity = cityId => {
 };
 
 export const fetchAllCountries = () => {
+  return fetch("https://stamp-book-api.herokuapp.com/api/countries")
+    .then(response => response.json())
+    .then(responseJson => responseJson.countries);
+};
+
+export const fetchCitiesByCountry = countryId => {
   return fetch(
-    `https://stamp-book-api.herokuapp.com/api/cities/${cityId}/landmarks`
+    `https://stamp-book-api.herokuapp.com/api/countries/${countryId}/cities`
   )
     .then(response => response.json())
     .then(responseJson => responseJson.cities);
