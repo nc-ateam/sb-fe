0d4d69f124232e7152a87429647726cd06da8c74
jbromley94 <jordanbromley94@gmail.com>
2018-08-23 17:18:26 +0100

    uplads to firebase

---
+++ config.js
@@ -0,0 +1,21 @@
+// const config = {
+//   apiKey: "AIzaSyBUxhpWJBFmxm0GDCjPZpHUuXQiNcRc1MQ",
+//   authDomain: "my-project-1531828203931.firebaseapp.com",
+//   databaseURL: "https://my-project-1531828203931.firebaseio.com",
+//   projectId: "my-project-1531828203931",
+//   storageBucket: "my-project-1531828203931.appspot.com",
+//   messagingSenderId: "758356405940"
+// };
+
+// export default config;
+
+export default {
+  FirebaseConfig:{
+    apiKey: "AIzaSyBUxhpWJBFmxm0GDCjPZpHUuXQiNcRc1MQ",
+    authDomain: "my-project-1531828203931.firebaseapp.com",
+    databaseURL: "https://my-project-1531828203931.firebaseio.com",
+    projectId: "my-project-1531828203931",
+    storageBucket: "my-project-1531828203931.appspot.com",
+    messagingSenderId: "758356405940"
+  }
+};
--- screens/GalleryScreen.js
+++ screens/GalleryScreen.js
@@ -1,30 +1,58 @@
 import React from "react";
 import { ImagePicker, Permissions } from "expo";
-import { Button, Image, View } from "react-native";
-
+import { Button, Image, View, Alert, Text } from "react-native";
+import * as firebase from "firebase";
 
 class GalleryScreen extends React.Component {
   state = {
-    image: null
+    image: null,
+    username: "michaelJackson",
+    firebase_URL: ""
   };
 
   pickImage = async () => {
     const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-    if(status === 'granted'){
-    let result = await ImagePicker.launchImageLibraryAsync({
-      allowsEditing: true,
-      aspect: [4, 3],
-      mediaTypes: "Images"
-    });
-  
+    if (status === "granted") {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [4, 3],
+        mediaTypes: "Images"
+      });
 
-    console.log(result, "<<<<<<<<<<<<");
-  
+      console.log(result, "<<<<<<<<<<<<");
 
-    if (!result.cancelled) {
-      this.setState({ image: result.uri });
+      // this.setState({ image: result.uri }).then(() => {
+      this.uploadImage(result.uri, "test-image")
+        .then(() => {
+          Alert.alert("SUCCESS");
+        })
+        .catch(error => {
+          console.log(error);
+          Alert.alert(error);
+        });
     }
-  }
+  };
+
+  uploadImage = async (uri, imageName) => {
+    console.log(`uploading image time`);
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status === "granted") {
+      const response = await fetch(uri);
+      let result = await Expo.Location.getCurrentPositionAsync();
+      const blob = await response.blob();
+      let ref = firebase
+        .storage()
+        .ref()
+        .child(`${this.state.username}` + `~${result.coords.longitude},${result.coords.latitude}~${this.state.username}`)
+        .getDownloadURL()
+        .then(url => {
+          console.log(url)
+          this.setState({ firebase_URL: url });
+
+        })
+
+      return ref.put(blob);
+    }
   };
 
   render() {
